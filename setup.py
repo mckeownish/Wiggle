3d4b73163f442fb3fd44f8b659fc3212381e6b77
mckeownish <j.mckeown124@gmail.com>
2025-10-27 15:11:13 +0000

    Multithread global lock ffs

--- setup.py
+++ setup.py
@@ -2,20 +2,31 @@ from setuptools import setup, find_packages, Extension
 from Cython.Build import cythonize
 import numpy
 
-# Define your Cython extensions
+
 extensions = [
-    Extension("wiggle.writhe", ["cython_src/writhe_c.pyx"]),
-    # Add other extensions if any
-    ]
+    Extension(
+        "wiggle.writhe", 
+        ["cython_src/writhe_c.pyx"],
+        extra_compile_args=['-fopenmp'],  # ADD THIS
+        extra_link_args=['-fopenmp'],      # ADD THIS
+        include_dirs=[numpy.get_include()]
+    ),
+]
     
 
 # Use cythonize on the extensions
 setup(
     name='wiggle',
-    version='0.5',
+    version='0.6',
     packages=find_packages(),
-    ext_modules=cythonize(extensions),
-    include_dirs=[numpy.get_include()],
+    ext_modules=cythonize(
+        extensions,
+        compiler_directives={
+            'language_level': "3",
+            'boundscheck': False,
+            'wraparound': False,
+        }
+    ),
     install_requires=[
         'numpy',
         'scipy',
@@ -26,7 +37,7 @@ setup(
         'tqdm',
         'selenium'
     ],
-    author='Josh McKeown',  # Add author information
-    author_email = 'josh.j.mckeown@durham.ac.uk',
-    description = 'Package used in my No Loose Ends Lab, where I explore a subfield of mathematics known as knot theory, specifically a metric of self entanglement referred to as the writhe. Im looking into its potential as a geometrical representation to help solve real world problems such as proteins folding!'
+    author='Josh McKeown',
+    author_email='josh.j.mckeown@durham.ac.uk',
+    description='Package used in my No Loose Ends Lab, where I explore a subfield of mathematics known as knot theory, specifically a metric of self entanglement referred to as the writhe. Im looking into its potential as a geometrical representation to help solve real world problems such as proteins folding!'
 )
